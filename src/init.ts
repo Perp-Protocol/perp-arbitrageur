b2f599f41973991373be549da90afa1e2f390176
Fortunate Rabbit <donghao0805@gmail.com>
2021-02-02 22:35:22 +0800

    remove unused code

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,8 @@
 import "reflect-metadata" // this shim is required
 import "source-map-support/register"
 import { configure } from "log4js"
-import { defaultTo } from "lodash"
 
-export const PROJECT_NAME = "perp-arbitrageur-v2"
+export const PROJECT_NAME = "perp-arbitrageur"
 
 // log4ts
 configure({
